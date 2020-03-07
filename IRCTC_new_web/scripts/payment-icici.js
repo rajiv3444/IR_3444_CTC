e1327e7309fc018e60edf3a1ebab2c04fabcecfe
Rajiv Bhardwaj <rajiv.bhardwaj@globant.com>
2020-03-07 14:18:16 +0530

    new payment icici

---
+++ IRCTC_new_web/scripts/payment-icici.js
@@ -0,0 +1,13 @@
+onclickPaymentTypes('VISA');
+document.getElementById("next").click();
+
+document.getElementById('bname').value = _cardHolderName;
+document.getElementById("cardnumber").value = _cardNumber;
+document.getElementById('expmonth').value = _cardExpiryMonth;
+
+document.getElementById('expyear').value = 2022-2020; // for 2022: value is 2
+document.getElementById('cvm_masked').value = 111;
+
+
+
+
