a211ceed613d2910481b2267b616df49796b2be4
Rajiv Bhardwaj <rajiv.bhardwaj@globant.com>
2020-03-09 17:09:25 +0530

    card fake data

---
+++ IRCTC_new_web/data/data-card.js
@@ -0,0 +1,8 @@
+_CARD_DETAIL = {
+    CardNumber: '1111222233334444',
+    CardExpiryMonth: 2,
+    CardExpiryYearYYYY: 2222,
+    CardCVV: '111',
+    CardHolderName: 'test kumar',
+    StaticPwd : 'dfsfD234'
+}
