396895ba1e1e00f1b06b3e52dd0b9bb1e4945fb7
Rajiv Bhardwaj <rajiv.bhardwaj@globant.com>
2020-03-09 17:02:52 +0530

    removed payment data

--- IRCTC_new_web/data/data-payment.js
+++
@@ -1,8 +0,0 @@
-_CARD_DETAIL = {
-    CardNumber: '56745674567',
-    CardExpiryMonth: 2,
-    CardExpiryYearYYYY: 2222,
-    CardCVV: '111',
-    CardHolderName: 'test ',
-    StaticPwd : 'dfsfD234'
-}
